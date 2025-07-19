0d7d4490bf2230f9d8c9662d286cdcecf2d39e65
OrsiniBr <udochukwugodwin13@gmail.com>
2025-07-20 00:43:07 +0100

    shift convertr for import

---
+++ fundMe/FundMe.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    function fund() public payable {
+        require( msg.value.getConversionRate() >= minimumUsd, "Not enough ETH");
+//         require(getConversionRate(msg.value) >= minimumUsd, "Not enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+     function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex] ;
+            addressToAmountFunded[funder] = 0;
+        }
+    }
+}
+
