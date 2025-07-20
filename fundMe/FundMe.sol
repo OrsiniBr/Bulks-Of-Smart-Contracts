62a78c934fc9414f5ea7016ce4714e8fc48dd7c1
OrsiniBr <udochukwugodwin13@gmail.com>
2025-07-20 02:53:20 +0100

    gas efficient code  and end of course

--- fundMe/FundMe.sol
+++ fundMe/FundMe.sol
@@ -9,6 +9,11 @@ contract FundMe {
     uint256 public minimumUsd = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+     address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require( msg.value.getConversionRate() >= minimumUsd, "Not enough ETH");
@@ -17,11 +22,25 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-     function withdraw() public {
+     function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex] ;
             addressToAmountFunded[funder] = 0;
         }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender must be owner");
+        _;
+    }
+    
+    receive() external payable {
+        fund();
+    }
+     fallback() external payable {
+        fund();
+     }
 }
 
